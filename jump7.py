90be2c21038fd09155e91fc9fe8df037540c9871
huangqiuyi <2301717629@qq.com>
2019-06-11 23:27:20 +0800

    jump 7

---
+++ jump7.py
@@ -0,0 +1,20 @@
+ in range(1,101):
+#       if i%7==0:
+#               continue
+#       elif i%10==7:
+#               continue
+#       elif  i//10==7:
+#               continue
+#       else:
+#               print(i)
+a=0
+while (a<100):
+        a+=1
+        if a%7==0:
+                continue
+        elif a//10==7:
+                continue
+        elif a%10==7:
+                continue
+        else:
+                print(a)
